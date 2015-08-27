41cfff3838b01983739087d745984e3aa37d9d70
HAMM3R67 <tcschauer@gmail.com>
2015-08-26 20:57:06 -0600

    trying to get access to the api to be able to post questions

--- controllers/MainController.js
+++ controllers/MainController.js
@@ -1,9 +1,12 @@
 var app = angular.module('parseQ');
 
-app.controller('mainController', function($scope, parseService){
+app.controller('mainController', function($scope, httpRequestInterceptor, parseService){
 	
 	$scope.postData = function(){
-		parseService.postQ($scope.question)
+		parseService.postQ($scope.addQuestion).then(function(result){
+			console.log(result.data);
+			
+		})
 	}
 	
 })
